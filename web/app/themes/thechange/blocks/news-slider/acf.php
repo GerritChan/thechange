dfbd6e43fdf3725dc414c0a0be1d146c4235c561
Gerritjan <gerritjan@bredewold.nl>
2024-01-14 16:28:20 +0100

    News slider and tekst image responsive

--- web/app/themes/thechange/blocks/news-slider/acf.php
+++ web/app/themes/thechange/blocks/news-slider/acf.php
@@ -3,4 +3,26 @@
 
 $block->addWysiwyg('Tekst', [
   'name' => 'text'
-]);
+])
+
+->addRepeater('reviews', [
+  'label' => 'Reviews',
+  'wrapper' => [
+     'width' => 100,
+  ],
+  'min' => 3,
+  'layout' => 'block'
+])
+
+->addText('name', [
+  'label' => 'Naam'
+])
+
+->addWysiwyg('Tekst', [
+  'name' => 'text',
+  'wrapper' => [
+     'width' => 100
+  ]
+])
+
+->endRepeater();

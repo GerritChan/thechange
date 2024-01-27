ea23d4ca0ffe20cf5ab2b8f600bcb8d606bb985b
Nadia Weijers <nadiaweijers@planet.nl>
2024-01-27 15:13:39 +0100

    fixes menu

--- web/app/themes/thechange/blocks/footer/view.php
+++ web/app/themes/thechange/blocks/footer/view.php
@@ -6,24 +6,27 @@
 
 <section class="footer container">
 
-    <div class="inner-wide top">
-        <a href="<?= get_home_url(); ?>" class="logo">
-            <?= file_get_contents(get_template_directory_uri() . '/build/images/logo.svg'); ?>
-        </a>
-
-        <div class="socials"> 
-            <a href="<?= get_field('instagram_link', 'option'); ?>" target="_blank"><?= file_get_contents( get_template_directory() . '/build/images/instagram.svg' ); ?></a>
-            <a href="<?= get_field('facebook_link', 'option'); ?>" target="_blank"><?= file_get_contents( get_template_directory() . '/build/images/facebook.svg' ); ?></a>
-            <a href="<?= get_field('linkedin_link', 'option'); ?>" target="_blank"><?= file_get_contents( get_template_directory() . '/build/images/linkedin.svg' ); ?></a>
-        </div>
-    </div>
 
     <div class="inner-wide bottom">
 
         <div class="col">
             <article>
-                <?php the_field('footer_content_one', 'option'); ?>
+            <a href="<?= get_home_url(); ?>" class="logo">
+            <?= file_get_contents(get_template_directory_uri() . '/build/images/logo.svg'); ?>
+            </a>
             </article>
+
+            <article>
+            <div class="socials"> 
+            <a href="<?= get_field('instagram_link', 'option'); ?>" target="_blank"><?= file_get_contents( get_template_directory() . '/build/images/instagram.svg' ); ?></a>
+            <a href="<?= get_field('facebook_link', 'option'); ?>" target="_blank"><?= file_get_contents( get_template_directory() . '/build/images/facebook.svg' ); ?></a>
+            <a href="<?= get_field('linkedin_link', 'option'); ?>" target="_blank"><?= file_get_contents( get_template_directory() . '/build/images/linkedin.svg' ); ?></a>
+        </div>
+            </article>
+
+  
+
+  
         </div>
 
         <div class="col">
